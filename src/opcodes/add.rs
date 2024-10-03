634226d29829e53ba778b36d4cfed725a097a0ae
maximopalopoli <mpalopoli@fi.uba.ar>
2024-10-03 13:01:02 -0300

    added add opcode tests

--- src/opcodes/add.rs
+++ src/opcodes/add.rs
@@ -40,3 +40,44 @@ pub fn add(instr: u16, regs: &mut [u16; 11]) {
 
     update_flags(dest_reg, regs);
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::add;
+    use super::super::super::registers;
+
+
+    #[test]
+    fn test_01() {
+        // Adding two number with registers makes the sum and lefts it in a third register
+
+        let mut regs: [u16; 11] = [0; 11];
+        regs[registers::RR1 as usize] = 1;
+        regs[registers::RR2 as usize] = 1;
+        
+        // This means 'Add RR1 and RR2 and put the result on RR3'
+        let instr: u16 = 0b0001011001000010;
+
+        add(instr, &mut regs);
+
+        assert_eq!(2, regs[registers::RR3 as usize]);
+    }
+
+    #[test]
+    fn test_02() {
+        // Adding one number with an imm5 makes the sum and lefts the result it in a third register
+
+        let mut regs: [u16; 11] = [0; 11];
+        regs[registers::RR1 as usize] = 3;
+        
+        // This means 'Add RR1 and an imm5 and put the result on RR3'
+        let instr: u16 = 0b0001011001100111;
+
+        add(instr, &mut regs);
+
+        assert_eq!(10, regs[registers::RR3 as usize]);
+    }
+
+    // Other tests: try an overflow
+}
