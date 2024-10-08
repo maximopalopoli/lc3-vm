82df4c39c7ac294b79824acd7429e225537f3fe8
maximopalopoli <mpalopoli@fi.uba.ar>
2024-10-08 12:46:53 -0300

    Add a trap test

--- src/hardware/opcodes/trap.rs
+++ src/hardware/opcodes/trap.rs
@@ -5,12 +5,12 @@ use std::{
 
 use crate::hardware::{registers, vm::VM};
 
-pub const TRAP_GETC: u16 = 0x20; /* get character from keyboard, not echoed onto the terminal */
-pub const TRAP_OUT: u16 = 0x21; /* output a character */
-pub const TRAP_PUTS: u16 = 0x22; /* output a word string */
-pub const TRAP_IN: u16 = 0x23; /* get character from keyboard, echoed onto the terminal */
-pub const TRAP_PUTSP: u16 = 0x24; /* output a byte string */
-pub const TRAP_HALT: u16 = 0x25; /* halt the program */
+pub const TRAP_GETC: u16 = 0x20;
+pub const TRAP_OUT: u16 = 0x21;
+pub const TRAP_PUTS: u16 = 0x22;
+pub const TRAP_IN: u16 = 0x23;
+pub const TRAP_PUTSP: u16 = 0x24;
+pub const TRAP_HALT: u16 = 0x25;
 
 pub fn trap(instr: u16, vm: &mut VM) {
     // Set the Reg7 to the PC value
@@ -98,10 +98,27 @@ pub fn trap(instr: u16, vm: &mut VM) {
 
 #[cfg(test)]
 mod tests {
-    /*
-       - Check the RR7 is set with the PC value
-       - Check that in the getc trap the value is set in RR0
-       - Could check that really reads/writes if I redirect the I/O
-       - Could run a thread and expect to close if I perform a HALT
-    */
+    use crate::{
+        hardware::{registers, vm::VM},
+        jmp::jmp,
+        trap::{trap, TRAP_OUT},
+    };
+
+    #[test]
+    fn test_01() {
+        //Check that the value of the PC is saved in R7
+        let mut vm = VM::new();
+
+        vm.update_register_value(registers::RR1, 16);
+
+        // This means 'Increment PC in the content in the base register'
+        let jmp_instr: u16 = 0b1100000001000000;
+        jmp(jmp_instr, &mut vm);
+
+        trap(TRAP_OUT, &mut vm);
+
+        assert_eq!(16, vm.get_register_value(registers::RR7));
+    }
+
+    // I imagine other tests, but for that cases i would have to mock i/o operations
 }
